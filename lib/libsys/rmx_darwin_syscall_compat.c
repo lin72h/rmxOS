3aaafc65b2ada34ef9244abdf62397b5e3315062
lin <me@linz.sh>
2026-06-19 21:30:29 +1200

    libsys: export Darwin task pid syscall aliases

--- lib/libsys/rmx_darwin_syscall_compat.c
+++ lib/libsys/rmx_darwin_syscall_compat.c
@@ -27,3 +27,21 @@ thread_switch(uint32_t thread_name, int option, uint32_t option_time)
 {
 	return (__sys_thread_switch(thread_name, option, option_time));
 }
+
+int
+task_name_for_pid(uint32_t target_tport, int pid, uint32_t *tn)
+{
+	return (__sys_task_name_for_pid(target_tport, pid, tn));
+}
+
+int
+task_for_pid(uint32_t target_tport, int pid, uint32_t *t)
+{
+	return (__sys_task_for_pid(target_tport, pid, t));
+}
+
+int
+pid_for_task(uint32_t t, int *pid)
+{
+	return (__sys_pid_for_task(t, pid));
+}
